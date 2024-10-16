7368fb80010a0f0aeae4ff8be1161862819e8872
Zulkan <zulkanzaqzuq@gmail.com>
2024-10-16 21:42:35 +0700

    Done with DHT

--- DHT-Sensor/src/main.cpp
+++ DHT-Sensor/src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define Type DHT11
 
-// put function declarations here:
-int myFunction(int, int);
+int sensePin = 2;
+DHT HT(sensePin,Type);
+float humidity;
+float tempC;
+float tempF;
+float tempK;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(9600);
+  HT.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+  tempF = (tempC * 9.0/5.0) + 32.0;
+  tempK = tempC + 273.0;
+
+  Serial.print("Humidity : ");
+  Serial.println(humidity);
+  delay(1000);
+
+  Serial.print("Temperature Celcius : ");
+  Serial.println(tempC);
+  delay(1000);
+  
+  Serial.print("Temperature Fahrenheit : ");
+  Serial.println(tempF);
+  delay(1000);
+
+  Serial.print("Temperature Kelvin : ");
+  Serial.println(tempK);
+  delay(1000);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
